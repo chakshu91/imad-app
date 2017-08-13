a8dff7bbfffb58c172bb73624380d95ab28fb0d0
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-13 09:42:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,62 +6,6 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleone={
-title:'article one|Chakshu Sharad',
-heading:'Article one',
-date:'9 Aug 2017',
-content:`
- <p>
-This is my first article.
-</p>
-<p>
-This is my first article. 
-</p>
-<p>
-This is my first article. 
-</p>
-};
-
-function createtemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=dta.content;
-}
-var htmltemplate =
- '<html>
-    <head>
-    <title>
-   ${title}
-    </title>
-   <link href="/ui/style.css" rel="stylesheet" />
-     </head>
-    <body>
-    <div class="container">
-    <div>
-    <hr/>
-    <a href="/">home</a>
-    </div>
-    <hr/>
-    <h3>
-   ${heading}
-    </h3>
-    <div>
-   ${date}
-    </div>
-    <div>
-    ${content}
-    </div>
-    </div>
-    </body>
-</html>
-';
-}
-return htmltemplate;
-}
-
-
-
 
 
 
@@ -84,13 +28,7 @@ app.get('/Article-three', function (req, res){
 
 
     
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
