1e10788e561b4e624a6ae7d735cd222aa0f389e7
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-13 09:45:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/Article-one', function (req, res){
-  res.send(createtemplate(articleone));
+ res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 app.get('/Article-two', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
