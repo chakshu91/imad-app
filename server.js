7b30982f000eb81549f2ded6210be1a20a1e0e56
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-14 16:38:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.get('/Article-two', function (req, res){
 app.get('/Article-three', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
+
+var counter = 0;
+app.get('/counter',function(req, res) {
+counter = counter + 1;
+res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res){
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
