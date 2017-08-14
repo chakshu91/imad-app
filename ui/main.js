463f6f35683ebfbc2f650b7fa50f01b16bc57a5c
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-14 13:19:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,8 @@ var element = document.getElementById('main-text'
 element.innerHTML ='MY NAME IS CHAKSHU SHARAD.';
 
 // Move the image
-var img =  document.getElementById('chakshu');
+var img =  document.getElementById('chakshu'
+);
 img.onclick = function () {
     img.style.marginleft='100px';
 };
