105256ab35892ece51ecded6feeb2c622d4cf642
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-15 11:43:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,6 +16,17 @@ img.onclick = function () {
 var interval = setInterval(moveRight, 50);
 };
 
+// Move the image
+var img = document.getElementById('chakshu');
+var marginLeft = 0;
+function moveRight () {
+marginLeft = marginLeft + 10;
+img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick  = function(){
+var interval = setInterval(moveRight,100);
+};
+
 
 
 // Counter code 
