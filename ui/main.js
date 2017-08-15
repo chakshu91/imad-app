589a77cd44ca0189263fb5b877f8feab5b23bb25
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-15 11:18:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,56 +1,30 @@
 console.log('Loaded!');
-
 // Change the text ot the main-text div
-
 var element = document.getElementById('main-text'
-
 );
-  
-
-  
+    
 element.innerHTML ='MY NAME IS CHAKSHU SHARAD.';
 
-
-
 // Move the image
-
 var img =  document.getElementById('chakshu');
-
 var marginLeft = 0;
-
 function moveRight () {
-
 marginLeft = marginLeft + 5;
-
 img.style.marginLeft = marginLeft + 'px';
-
 }
 img.onclick = function () {
-
 var interval = setInterval(moveRight, 50);
-
 };
-
 
 var img =  document.getElementById('chakshu2');
-
 var marginLeft = 0;
-
 function moveRight () {
-
 marginLeft = marginLeft + 5;
-
 img.style.marginLeft = marginLeft + 'px';
-
 }
-
 img.onclick = function () {
-
 var interval = setInterval(moveRight, 50);
-
 };
-
-
 
 
 
