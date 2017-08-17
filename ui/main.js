a4f5da92814a4341a7643a0bc7d88978f4a7f081
chakshu91 <30751037+chakshu91@users.noreply.github.com>
2017-08-17 20:10:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,19 @@ img.style.marginLeft = marginLeft + 'px';
 img.onclick = function () {
  var interval = setInterval(moveRight, 100);
  };
+ 
+ // Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+
+// Make a request to the counter endpoint
+
+// Capture the response and store it in a variable 
+
+// Render the variable in the correct span 
+counter = counter + 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+};
